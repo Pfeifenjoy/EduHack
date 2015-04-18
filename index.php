9c79a7d552b18ee759e51d43a924019ceeca553c
Jess <kaese-chan@gmx.net>
2015-04-18 14:58:23 +0200

    start page css

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype HTML>
+<!DOCTYPE html>
 
 <html>
     <head>
@@ -11,33 +11,33 @@
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
-        <link rel="stylesheet" href="css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/main.css" type="text/css">
     </head>
     <body>
         <nav class="navbar navbar-default">
           <div class="container-fluid">
             <div class="navbar-header">
               <a class="navbar-brand" href="#">
-                <img alt="Brand" src=""/>
+                <img alt="Brand" src="img/logo_klein.png"/>
               </a>
-            </div>
-            <div class="navbar navbar-nav navbar-left">
-              <button type="button" class="btn btn-default navbar-btn">Search</button>
-            </div>
-            <div class="navbar navbar-nav navbar-right">
-                <button type="button" class="btn btn-default navbar-btn">Login</button>
-                <button type="button" class="btn btn-default navbar-btn">Register</button>
+                <ul>
+                    <li><a>Suchen</a></li>
+                    <li><a>Login</a></li>
+                    <li><a>Registrieren</a></li>
+                </ul>
             </div>
           </div>
         </nav>
         <section class="container">
-            <img id="mainLogo" alt="SolveIt" />
-            <div class="input-group" id="mainSearch">
-                <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                        <button class="btn btn-default" type="button">Go!</button>
-                    </span>
+            <div id="mainsearch">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Gib hier dein Problem ein!">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                    </input>
+                </div><!-- /input-group -->
             </div>
             <?php
                 include("sites/login.php");

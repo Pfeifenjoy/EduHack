21ab32f1cfacdf78d87173c389f4698d075b2606
NE4Y <slindner1996@gmail.com>
2015-04-18 15:15:53 +0200

    changes to home

---
+++ sites/home.php
@@ -0,0 +1,9 @@
+ <div class="input-group" id="mainSearch">
+                <input type="text" class="form-control" placeholder="Gib hier dein Problem ein!">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">Los!</button>
+                    </span>     
+            </div>
+            <div class="own-problem">
+                <p>… oder <a href="">erstelle dein eigenes Problem</a>.</p>
+            </div>

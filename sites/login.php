5c032a12734274900f2c0da73957c3e74f880b78
Arwed Mett <arwed.mett@googlemail.com>
2015-04-18 13:18:16 +0200

    started login / register

---
+++ sites/login.php
@@ -0,0 +1,17 @@
+
+<div class="login">
+  <h1>Login to Web App</h1>
+  <form method="post" action="index.html">
+    <div class="input-group">
+        <span class="input-group-addon" id="basic-addon1">@</span>
+        <input name="id" type="text" class="form-control" placeholder="Username or Email" aria-describedby="basic-addon1">
+    </div>
+
+    <div class="input-group">
+        <span class="input-group-addon glyphicon glyphicon-lock" id="basic-addon1"></span>
+        <input name="pw" type="text" class="form-control" placeholder="Password" aria-describedby="basic-addon1">
+    </div>
+    
+    <input class="btn btn-primary" type="submit" value="Login" />
+  </form>
+</div>

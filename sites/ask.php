4ecb0a5493e819a1a64c48e0e2f4f995f22c8952
Tobias Dorra <public.tobias@outlook.com>
2015-04-19 01:08:08 +0200

    Loading experts via ajax

--- sites/ask.php
+++ sites/ask.php
@@ -1,16 +1,6 @@
-<textarea class="form-control" rows="2" placeholder="Wie kann dir geholfen werden? (#hashtags erlaubt)"></textarea>
+<textarea id="question" class="form-control" rows="2" placeholder="Wie kann dir geholfen werden? (#hashtags erlaubt)"></textarea>
 
 <div id="matching-persons" class="row">
-	<div class="col-md-4 person-container">
-		<div class="person thumbnail clearfix">
-			<img class="pull-left" src="http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c00001?default=retro"/>
-			<section>
-				<h3>TheMasterMinder</h3>
-				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
-				<button class="btn btn-primary pull-right">Hilf mir</button>
-			</section>
-		</div>
-	</div>
 	<div class="col-md-4 person-container">
 		<div class="person thumbnail clearfix">
 			<img class="pull-left" src="http://www.gravatar.com/avatar/305e460b479e2e5b48aec07710c00001?default=retro"/>
@@ -50,3 +40,18 @@
 	<p>Keinen passenden Ansprechpartner gefunden?</p>
 	<button class="btn btn-primary">Stelle deine Frage öffentlich</button>
 </div>
+
+<template id="expert-preview">
+	<div class="col-md-4 person-container">
+		<div class="person thumbnail clearfix">
+			<img class="pull-left" src="http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c00001?default=retro"/>
+			<section>
+				<h3>TheMasterMinder</h3>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+				<button class="btn btn-primary pull-right">Hilf mir</button>
+			</section>
+		</div>
+	</div>
+</template>
+
+<script src="js/ask.js"></script>

fb0521db27cc4d81766ac5369a7e7b250af78dd0
Jess <kaese-chan@gmx.net>
2015-04-18 20:03:49 +0200

    home text geaendert

--- sites/home.php
+++ sites/home.php
@@ -1,11 +1,11 @@
  <div class="input-group" id="mainSearch">
-                <input type="text" class="form-control" placeholder="Gib hier dein Problem ein!">
+                <input type="text" class="form-control" placeholder="Suche hier nach Problemen, Nutzern oder Keywords">
                     <span class="input-group-btn">
                         <button class="btn btn-default" type="button">Los!</button>
                     </span>     
             </div>
             <div class="own-problem">
-                <p>… oder <a href="">erstelle dein eigenes Problem</a>.</p>
+                <p>… oder <a href="">erstelle ein neues Problem</a>.</p>
             </div>
 
 <div class="row" id="searchResults">

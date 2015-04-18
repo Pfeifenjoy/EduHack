07513010e0c3dc0ac4fb0fc77b97a312bc7dd48c
Arwed Mett <arwed.mett@googlemail.com>
2015-04-18 15:14:10 +0200

    small changes

--- index.php
+++ index.php
@@ -24,7 +24,8 @@ DBHandler::initDB();
         <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
+        <script src="js/navi.js"></script>
+        <script src="js/page.js"></script>
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
---
+++ js/page.js
@@ -0,0 +1,68 @@
+/* ------------------------- 
+Page.js
+Copyright: Steffen Lindner
+www.ne4y-dev.de
+----------------------------- */
+$(document).ready(function () {
+    var sLink = window.location.hash;
+    var goLink = sLink.substring(sLink.indexOf("=") + 1);
+
+    if (goLink != undefined) {
+        $('nav ul li').removeClass('active');
+    }
+
+    if (sLink.length > 0 && goLink.length > 0) {
+        $('nav ul li a[href="?s=' + goLink + '"]').parent().addClass('active');
+        $('section#content').empty();
+        $('section#content').html('<div id="loading"><img src="img/loader.gif" alt="laden" id="loader" /></div>');
+        $('img#loader').show();
+        $.ajax({
+            url: 'request.php?s=' + goLink,
+            success: function (result) {
+                $('img#loader').hide();
+                $('section#content').html(result);
+            }
+        });
+    } else {
+        //$('nav ul li:first-child').addClass("active");
+    }
+
+    $('a').click(function () {		
+        var href = $(this).attr('href');
+        var rLink = href.substring(href.indexOf("=") + 1);
+        $('section#content').empty();
+        $('section#content').html('<div id="loading"><img src="img/loader.gif" alt="laden" id="loader" /></div>');
+        $('img#loader').show();
+        $.ajax({
+            url: 'request.php?s=' + rLink,
+            success: function (result) {
+                $('img#loader').hide();
+                $('section#content').html(result);
+                window.location.hash = "s=" + rLink;				
+            }			
+        });
+		
+		return false;
+    });
+	
+	
+});
+
+$(document).delegate("form", "submit", function () {
+    var href = $(this).attr('action');
+    var rLink = href.substring(href.indexOf("=") + 1);
+    var information = $('form').serialize();
+    $('section#content').html('<div id="loading"><img src="img/loader.gif" alt="laden" id="loader" /></div>');
+    $.ajax({
+        url: 'request.php?s=' + rLink,
+        type: 'POST',
+        data: information,
+        success: function (result) {
+            $('img#loader').hide();
+            $('section#content').html(result);
+            window.location.hash = "s=" + rLink;
+        }
+    });
+    
+    return false;
+});

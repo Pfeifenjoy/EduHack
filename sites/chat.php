063b7087d3140d33139432ff824fdba6c67a2c2e
NE4Y <slindner1996@gmail.com>
2015-04-18 22:20:52 +0200

    update

--- sites/chat.php
+++ sites/chat.php
@@ -1,11 +1,15 @@
 <?php
 $c = new Chat();
 $d = $c->getChatById($_GET['id']);
+$chatMessages = $c->getChatMessages($_GET['id']);
+
+var_dump($chatMessages);
+
 ?><section class="chat-background">
           <div class="container">
             <div class="chat">
                 <div class="col-md-9">
-                  <div class="col-md-12 post clear">
+                  <!--<div class="col-md-12 post clear">
                     <div class="col-md-2">
                       <div class="user1 user-data">
                         <img src="img/login_bild.png" alt="Benutzerbild" />
@@ -16,9 +20,9 @@ $d = $c->getChatById($_GET['id']);
                     <div class="col-md-10 content">
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
                   </div>
-                </div> 
+                </div> -->
                   
-                 <div class="col-md-12 post clear">
+                <!-- <div class="col-md-12 post clear">
                     <div class="col-md-10 content">
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
                   </div>
@@ -29,7 +33,7 @@ $d = $c->getChatById($_GET['id']);
                         <a href="">Username</a>
                       </div>
                     </div>
-                </div> 
+                </div> -->
                     <!--
                   
                   <div class="col-md-12 post clear">

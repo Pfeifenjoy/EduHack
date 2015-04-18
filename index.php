2d537522e74eddb71360675bec53d5741112d00d
Arwed Mett <arwed.mett@googlemail.com>
2015-04-18 12:22:16 +0200

    created search bar

--- index.php
+++ index.php
@@ -13,6 +13,31 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     </head>
     <body>
-    
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">
+                <img alt="Brand" src=""/>
+              </a>
+            </div>
+            <div class="navbar navbar-nav navbar-left">
+              <button type="button" class="btn btn-default navbar-btn">Search</button>
+            </div>
+            <div class="navbar navbar-nav navbar-right">
+                <button type="button" class="btn btn-default navbar-btn">Login</button>
+                <button type="button" class="btn btn-default navbar-btn">Register</button>
+            </div>
+          </div>
+        </nav>
+        <section class="container">
+            <div class="jumbotron">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                </div><!-- /input-group -->
+            </div>
+        </section>
     </body>
 </html>

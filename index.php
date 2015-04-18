a5fbdbba48909c95389daed4909f99e67a15a80d
NE4Y <slindner1996@gmail.com>
2015-04-18 13:36:39 +0200

    changes

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<!doctype HTML>
+<?php
+session_start();
+
+require("inc/config.inc.php");
+
+function __autoload($class_name) {
+    include 'classes/'.strtolower($class_name) . '.class.php';
+}
+
+
+DBHandler::initDB();
+?>
+<!DOCTYPE html>
 
 <html>
     <head>
@@ -39,8 +51,19 @@
                         <button class="btn btn-default" type="button">Go!</button>
                     </span>
             </div>
-            <?php
-                include("sites/login.php");
+            <?php	
+            if(isset($_GET['s']) && !empty($_GET['s'])) {
+                if(file_exists(realpath('./sites/')."/".$_GET['s'].".php")) {
+                    include(realpath('./sites/')."/".$_GET['s'].".php");
+                }
+                else {
+                    include(realpath('./sites/').'/404.php');
+                }
+            } 
+	       else {
+             include(realpath('./sites/register.php'));
+           }
+	
             ?>
         </section>
     </body>

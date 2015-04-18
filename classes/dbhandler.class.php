24c724286c3e322919f7abecd67eb0d959e28ae6
NE4Y <slindner1996@gmail.com>
2015-04-18 12:25:15 +0200

    dbhandler

---
+++ classes/dbhandler.class.php
@@ -0,0 +1,16 @@
+<?php
+require("db.class.php");
+// DB Handler
+class DBHandler {
+    public static $db;
+    
+    public static function initDB() {
+        self::$db = new DB(HOST, DB, USER, PW);
+    }
+    
+    public static function getDB() {
+        return self::$db;
+    }
+}
+
+?>

cabd801382062f67d8a2de816a7c4cb9e734fb37
Arwed Mett <arwed.mett@googlemail.com>
2015-04-18 17:43:03 +0200

    loading

--- js/page.js
+++ js/page.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     if (sLink.length > 0 && goLink.length > 0) {
         $('nav ul li a[href="?s=' + goLink + '"]').parent().addClass('active');
         $('section.container').empty();
-        $('section.container').html('<div id="loading"><img src="img/loader.gif" alt="laden" id="loader" /></div>');
+        $('section.container').html('<div id="loading"><img src="img/loading.gif" alt="laden" id="loader" /></div>');
         $('img#loader').show();
         $.ajax({
             url: 'request.php?s=' + goLink,
@@ -31,7 +31,7 @@ $(document).ready(function () {
         var href = $(this).attr('href');
         var rLink = href.substring(href.indexOf("=") + 1);
         $('section.container').empty();
-        $('section.container').html('<div id="loading"><img src="img/loader.gif" alt="laden" id="loader" /></div>');
+        $('section.container').html('<div id="loading"><img src="img/loading.gif" alt="laden" id="loader" /></div>');
         $('img#loader').show();
         $.ajax({
             url: 'request.php?s=' + rLink,
@@ -52,7 +52,7 @@ $(document).delegate("form", "submit", function () {
     var href = $(this).attr('action');
     var rLink = href.substring(href.indexOf("=") + 1);
     var information = $('form').serialize();
-    $('section.container').html('<div id="loading"><img src="img/loader.gif" alt="laden" id="loader" /></div>');
+    $('section.container').html('<div id="loading"><img src="img/loading.gif" alt="laden" id="loader" /></div>');
     $.ajax({
         url: 'request.php?s=' + rLink,
         type: 'POST',

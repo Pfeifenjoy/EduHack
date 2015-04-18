81ec606a18b8b46107dbec4ea6406fd4f632039d
Jess <kaese-chan@gmx.net>
2015-04-18 18:35:06 +0200

    problem.php edits

--- sites/problem.php
+++ sites/problem.php
@@ -12,6 +12,10 @@
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
                   </div>
                   
+                  <div class="headline mini">
+                  <p>Dein Problem wurde erfolgreich eingestellt. Du kannst nun warten, bis jemand sich bereiterklärt, dein Problem zu bearbeiten. Alternativ kannst du es auch Nutzern vorschlagen.<br/>
+                    Diese Nutzer können dein Problem vielleicht beantworten:</p>
+                    </div>
                   <div class="col-sm-4 recommended-wrapper">
                     <div class="recommended">
                       <div class="user-img">

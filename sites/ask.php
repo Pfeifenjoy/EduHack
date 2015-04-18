c5f0199951e81f95e980077e3e1c0792f73ceeb9
Tobias Dorra <public.tobias@outlook.com>
2015-04-18 23:41:12 +0200

    Design für die "Fragen"-Seite

---
+++ sites/ask.php
@@ -0,0 +1,52 @@
+<textarea class="form-control" rows="2" placeholder="Wie kann dir geholfen werden? (#hashtags erlaubt)"></textarea>
+
+<div id="matching-persons" class="row">
+	<div class="col-md-4 person-container">
+		<div class="person thumbnail clearfix">
+			<img class="pull-left" src="http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c00001?default=retro"/>
+			<section>
+				<h3>TheMasterMinder</h3>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+				<button class="btn btn-primary pull-right">Hilf mir</button>
+			</section>
+		</div>
+	</div>
+	<div class="col-md-4 person-container">
+		<div class="person thumbnail clearfix">
+			<img class="pull-left" src="http://www.gravatar.com/avatar/305e460b479e2e5b48aec07710c00001?default=retro"/>
+			<section>
+				<h3>Cheryll</h3>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+				<button class="btn btn-primary pull-right">Hilf mir</button>
+			</section>
+		
+		</div>
+	</div>
+	<div class="col-md-4 person-container">
+		<div class="person thumbnail clearfix">
+			<img class="pull-left" src="http://www.gravatar.com/avatar/405e460b479e2e5b48aec07710c00001?default=retro"/>
+			<section>
+				<h3>LordOfTheRings</h3>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+				<button class="btn btn-primary pull-right">Hilf mir</button>
+			</section>
+		
+		</div>
+	</div>
+	<div class="col-md-4 person-container">
+		<div class="person thumbnail clearfix">
+			<img class="pull-left" src="http://www.gravatar.com/avatar/505e460b479e2e5b48aec07710c00001?default=retro"/>
+			<section>
+				<h3>SomeUsrName</h3>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+				<button class="btn btn-primary pull-right">Hilf mir</button>
+			</section>
+		
+		</div>	
+	</div>
+</div>
+
+<div id="ask-public" class="thumbnail">
+	<p>Keinen passenden Ansprechpartner gefunden?</p>
+	<button class="btn btn-primary">Stelle deine Frage öffentlich</button>
+</div>

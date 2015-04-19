a1bd1ad2d3c75a6438a9d338620996884e7ae4b5
NE4Y <slindenr1996@gmail.com>
2015-04-19 07:51:17 +0200

    update

--- sites/shareProblem.php
+++ sites/shareProblem.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_SESSION['username'])) {
-    if(DBHandler::getDB()->query("INSERT INTO questions (question, author, status) VALUES (?,?,?)", array($_POST['question'], $_SESSION['user_id'], 1))) {
+    if(DBHandler::getDB()->query("INSERT INTO questions (question, author, status) VALUES (?,?,?)", array($_POST['question'], $_SESSION['user_id'], 0))) {
     echo'<p class="success">Dein Problem wurde online gestellt.</p>';
     }
     

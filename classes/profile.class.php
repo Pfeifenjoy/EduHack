50d9eea211e9766eaec1e825bb8302a5c8046a89
Tobias Dorra <public.tobias@outlook.com>
2015-04-19 07:18:14 +0200

    JSON-Interface für Personensuchen

--- classes/profile.class.php
+++ classes/profile.class.php
@@ -1,5 +1,5 @@
 <?php
-class Profile
+class Profile implements JsonSerializable
 {
 	//---- private -------------------------------------------------------------------------------
 	private $id;
@@ -37,12 +37,24 @@ class Profile
 		return $this->creationDate;
 	}
 	
-	
-	
 	public function getPictureUrl()
 	{
 		$hash = md5(strtolower(trim($this->email)));
 		return "http://www.gravatar.com/avatar/" . $hash . "?default=retro&s=250";
+	}
+	
+	public function getNrOfHashtags()
+	{
+		return DBHandler::getDB()->num_rows("SELECT * FROM account_hashtags WHERE account = ?", array($this->id));
+	}
+	
+	public function jsonSerialize() {
+		return array(
+				"id" => $this->id,
+				"name" => utf8_encode($this->username),
+				"pic" => $this->getPictureUrl(),
+				"description" => "Lorem Ipsum Dolor Sit Amet..."
+		);
 	}
 	
 	//---- public --------------------------------------------------------------------------------
@@ -77,5 +89,69 @@ class Profile
 		{
 			die();
 		}
+	}
+	
+	/**
+	 * @param Hashtag $hashtag
+	 * @return array:Profile
+	 */
+	public static function findManyByCompetence(Hashtag $hashtag)
+	{
+		$db_result = DBHandler::getDB()->fetch_all("SELECT * FROM account WHERE id IN (SELECT account FROM account_hashtags WHERE hashtag = ?)", array($hashtag->getId()));
+		$result = array();
+		foreach ($db_result as $entry) {
+			$result[] = new Profile($entry);
+		}
+		return $result;
+	}
+	
+	public static function findManyByKeywords($keywords)
+	{
+		// tags
+		$hashtags = array();
+		foreach ($keywords as $keyword) {
+			$levenshtein_hashtags = Hashtag::findManyByLevenshtein($keyword);
+			$hashtags = array_merge($hashtags, $levenshtein_hashtags);
+		}
+		
+		// profiles with these tags
+		$profiles = array();
+		foreach ($hashtags as $hashtag)
+		{
+			$hashtag_profiles = Profile::findManyByCompetence($hashtag);
+			foreach ($hashtag_profiles as $hashtag_profile) {
+				if(isset($profiles[$hashtag_profile->getId()]))
+				{
+					$profiles[$hashtag_profile->getId()]["count"]++;
+				}
+				else
+				{
+					$profiles[$hashtag_profile->getId()] = array("count" => 1, "profile" => $hashtag_profile);
+				}
+			}
+		}
+		
+		// calculate the ranking = nr_of_matching_tags / nr_of_all_tags
+		foreach ($profiles as &$profile) {
+			$profile["ranking"] = $profile["count"] / $profile["profile"]->getNrOfHashtags();
+		}
+		
+		// sort for count/nr_tags
+		usort($profiles, function ($a, $b){
+			if($a["ranking"] == $b["ranking"])
+			{
+				return 0;
+			}
+			return ($a["ranking"] > $b["ranking"])? -1 : 1;
+		});
+		
+		// summarize
+		$result = array();
+		foreach ($profiles as $profile) {
+			$result[] = $profile["profile"];
+		}
+		
+		//
+		return $result;
 	}
 }
